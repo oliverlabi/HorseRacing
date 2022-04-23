16da09070ddebf1fe4358c52e1c7d6b36c9a4811
Oliver <olku211@tlu.ee>
2022-04-23 23:22:37 +0300

    Created more fields for create-race page

--- src/pages/CreateRacePage.js
+++ src/pages/CreateRacePage.js
@@ -1,18 +1,52 @@
 import '../App.less';
 import './CreateRacePage.css'
-import { Form, Input, Button } from 'antd';
+import moment from 'moment';
+import { useState } from 'react';
+import { Form, Input, Button, DatePicker, Select } from 'antd';
 
-const onFinish = (values) => {
-    console.log(values);
-}
+function CreateRacePage(){
+    const [currentDate, setCurrentDate] = useState(moment());
 
-function createRacePage(){
+    const onFinish = (values) => {
+        console.log(values);
+    }
+
+    function getDisabledHours(){
+        var hours = [];
+        if(currentDate.date() === moment().date()){
+            for(var i = 0; i < moment().hour(); i++){
+                hours.push(i);
+            }
+        }
+        return hours;
+    }
+
+    function getDisabledMinutes(selectedHour){
+        var minutes = [];
+        if (selectedHour === moment().hour() && currentDate.date() === moment().date()){
+            for(var i =0; i < moment().minute(); i++){
+                minutes.push(i);
+            }
+        }
+        return minutes;
+    }
+
+    function disabledDate(current){
+        return current < moment().startOf('day');
+    }
+
     return(
         <div className='sub-background'>
             <h1>Create a race</h1>
+            <br/>
             <Form
                 onFinish={onFinish}
-                
+                wrapperCol={{
+                    span: 10,
+                }}
+                labelCol={{
+                    span: 10,
+                }}
             >
                 <Form.Item
                     label="Name your race:"
@@ -20,7 +54,7 @@ function createRacePage(){
                     rules={[
                         {
                             required: true,
-                            message: 'Filed must not be empty!',
+                            message: 'Fields must not be empty!',
                         }
                     ]}
                 >
@@ -32,11 +66,50 @@ function createRacePage(){
                     rules={[
                         {
                             required: true,
-                            message: 'Filed must not be empty!',
+                            message: 'Fields must not be empty!',
                         }
                     ]}
                 >
                     <Input/>
+                </Form.Item>
+                <Form.Item
+                    label="Start time:"
+                    name="timeStart"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Fields must not be empty!',
+                        }
+                    ]}
+                >
+                    <DatePicker 
+                        showTime
+                        showNow={false}
+                        format="DD.MM.YYYY HH:mm" 
+                        minuteStep={5}
+                        onSelect={setCurrentDate}
+                        disabledDate={disabledDate}
+                        disabledHours={getDisabledHours}
+                        disabledMinutes={getDisabledMinutes}
+                    />
+                </Form.Item>
+                <Form.Item
+                    label="Track:"
+                    name="trackChoice"
+                    
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Fields must not be empty!',
+                        }
+                    ]}
+                >
+                    <Select placeholder="Choose a track">
+                        <Select.Option value="500m-g">Gravel 500m</Select.Option>
+                        <Select.Option value="1000m-g">Gravel 1km</Select.Option>
+                        <Select.Option value="500m-g">Asphalt 500m</Select.Option>
+                        <Select.Option value="500m-g">Asphalt 1km</Select.Option>
+                    </Select>
                 </Form.Item>
             </Form>
         </div>
@@ -44,4 +117,4 @@ function createRacePage(){
     );
 }
 
-export default createRacePage;
+export default CreateRacePage;
