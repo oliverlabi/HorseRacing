d4bde7ed8db5e9a9ce6075f5a0eea031db19eb55
Oliver <olku211@tlu.ee>
2022-05-05 22:28:47 +0300

    README changes

---
+++ index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const express = require('express');
+const app = require("./backend-node/src/server");
+const PORT = process.env.PORT || 8000
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, "frontend-react/build")))
+if (process.env.NODE_ENV === "production") {
+    app.get(/^((?!(api)).)*$/, (req, res) => {
+        res.sendFile(path.join(__dirname, '/frontend-react/build/index.html'));
+    });
+}
+
+mongoose
+    .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server started on PORT ${PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+        process.exit(1)
+})
